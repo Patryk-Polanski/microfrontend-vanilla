fba2554fb50fa196c7259d7bd8dd779f9485b1c2
patryk-polanski <pat.polanski@gmail.com>
2026-02-07 15:39:11 +0100

    create webpack dev server, display list of products on screen

--- ecomm/products/src/index.js
+++ ecomm/products/src/index.js
@@ -2,11 +2,11 @@ import faker from "faker";
 
 let products = "";
 
-for (let i = 0; i < 3; i++) {
+for (let i = 0; i < 5; i++) {
   const name = faker.commerce.productName();
   products += `
-        <div>${name}</div>
+        <li>${name}</li>
     `;
 }
 
-console.log("products", products);
+document.querySelector("#dev-products").innerHTML = products;
