555458bcc67648bc34aba668bc9e300de7fef8c6
patryk-polanski <pat.polanski@gmail.com>
2026-02-09 21:46:18 +0100

    refactor: create a mount function for products, pass in target element from container

--- ecomm/products/src/bootstrap.js
+++ ecomm/products/src/bootstrap.js
@@ -1,12 +1,26 @@
 import faker from "faker";
 
-let products = "";
+const mount = (el) => {
+  let products = "";
 
-for (let i = 0; i < 5; i++) {
-  const name = faker.commerce.productName();
-  products += `
+  for (let i = 0; i < 5; i++) {
+    const name = faker.commerce.productName();
+    products += `
         <li>${name}</li>
     `;
+  }
+
+  el.innerHTML = products;
+};
+
+// check if we are in development mode and are developing in isolation/locally
+if (process.env.NODE_ENV === "development") {
+  // assumption that the container will not have an element with the below id, only our local html file will
+  const el = document.querySelector("#dev-products");
+  if (el) {
+    // we are probably running in isolation and not through the container
+    mount(el);
+  }
 }
 
-document.querySelector("#dev-products").innerHTML = products;
+export { mount };
